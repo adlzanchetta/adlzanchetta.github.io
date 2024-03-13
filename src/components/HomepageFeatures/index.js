5d61311a090a9f29f4c06d9f0f7dd137586997e8
adlzanchetta <adlzanchetta@gmail.com>
2024-03-13 15:08:43 -0400

    Enhance skills listing in the landing page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,41 +6,68 @@ const FeatureList = [
   {
     title: 'Frontend',
     Svg: require('@site/static/img/landing-page/frontend_montains.svg').default,
-    description: (
-      <>
-        ReactJS, CSS frameworks, responsive design, optimized web maps...
-      </>
-    ),
+    skills: [
+      ['Javascript', 'JQuery'],
+      ['ReactJS', 'Recoil'],
+      ['Responsive design'],
+      ['Web maps', 'Bootstrap'],
+      ['TailwindCSS', 'HTML5'],
+      ['Showing what we get!']
+    ],
   },
   {
     title: 'Backend',
     Svg: require('@site/static/img/landing-page/backend_bricks.svg').default,
-    description: (
-      <>
-        Python, NodeJS and TypeScript APIs, SQL databases, C and C++ programming...
-      </>
-    ),
+    skills: [
+      ['Python', 'NodeJS'],
+      ['Flask', 'ExpressJS'],
+      ['REST APIs', 'Linux'],
+      ['SQL', 'C / C++'],
+      ['Java', 'PM2'],
+      ['I got your back!']
+    ]
   },
   {
     title: 'Altogether',
     Svg: require('@site/static/img/altogether_tower.svg').default,
-    description: (
-      <>
-        Docker, software design and engineering, continuous integration...
-      </>
-    ),
+    skills: [
+      ['Docker', 'CI/CD'],
+      ['Design patterns'],
+      ['Software engineering'],
+      ['ML / AI', 'Git'],
+      ['Unit / end-to-end tests'],
+      ["There is 'I' in 'integration'!"]
+    ],
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Svg, title, skills}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
-      <div className="text--center padding-horiz--md">
+      <div className={`text--center padding-horiz--md ${styles.skillsVault}`} >
         <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
+        { skills && (
+          <div className={styles.skillsBox}> {
+            skills.map((skill, idx) => (
+              <div key={idx} className={styles.skillsLine}>
+                {
+                  (skill.length === 1) ?
+                  (<div className={styles.skill}>{skill[0]}</div>) : 
+                  (
+                    <>
+                      <div className={styles.skill}>{skill[0]}</div>
+                      <div className={styles.skill}>{skill[1]}</div>
+                    </>
+                  )
+                }
+              </div>
+            ))
+          }
+          </div>
+        )}
       </div>
     </div>
   );
